6f795c73f15f400b06bd55062954b16d10396927
alvarez.freddie@ic.edu <alvarez.freddie@ic.edu>
2025-11-12 11:06:52 -0600

    added bcrypt and User

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { db, Project, Task } = require('./database/setup');
+const bcrypt = require('bcryptjs');
+const { db, Project, Task, User } = require('./database/setup');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
